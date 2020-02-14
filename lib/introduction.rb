b9c46e6a219c331211628dae1d15a587bce9d8ee
Bala Luguja <bluguja@gmail.com>
2020-02-14 22:23:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,13 @@
-def introduction(name)
-  puts "Hi,my name is #{name}. "
-end
+  def introduction(name)
+    puts "Hi,my name is #{name}. "
+  end
  
 introduction("Josh")
 
-def introduction_with_language(name,language)
-  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+  def introduction_with_language(name,language)
+    puts "Hello, #{name}. We heard you are a great #{language} programmer."
 
+  end
 end
  
 introduction_with_language("Dan", "Ember.js")
