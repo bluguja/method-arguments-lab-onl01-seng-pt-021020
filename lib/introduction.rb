dc4e28c8bfe8360e4a1e61ce2e120ef83e253e2a
Bala Luguja <bluguja@gmail.com>
2020-02-14 22:15:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,4 +4,9 @@ end
  
 introduction("Sophie")
 
+def introduction_with_language(name,language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+ 
+introduction_with_language("Sophie", "Ruby")
 
