ed083fded04d25f06f35832edec2ec0d09b1ef5e
Bala Luguja <bluguja@gmail.com>
2020-02-14 22:26:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,1 @@
-  def greeting_programmer(name, language)
-  puts "Hello, #{name}. We heard you are a great #{language} programmer."
-end
- 
-greeting_programmer("Sophie", "Ruby")
-# > Hello, Sophie. We heard you are a great Ruby programmer.
- 
-greeting_programmer("Steven", "Elixir")
-# > Hello, Steven. We heard you are a great Elixir programmer.
+  
